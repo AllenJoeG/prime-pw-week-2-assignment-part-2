b815118626c38b916cd4f2f95f450b6333ec285f
AllenJoeG <allen.joeg@gmail.com>
2021-10-01 14:11:57 -0500

    Update 0.3 Further revision, some clarifications and formatting.

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -5,12 +5,12 @@
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
 
-
+// (OOPS SORRY REALIZED I EDITED YOUR EXAMPLE HERE)
 // 0. WRITE YOUR DESCRIPTION HERE
-// //EXAMPLE. We make a variable called number and set it to 1 as a number.
-// Then we increment the number variable. Number is now 2.
+// //EXAMPLE. We declare a variable named 'number' and set its value to 1.
+// Then we increment the number variable. now var 'number' = 2.
 // We check if number is greater than or equal to 2. 2 is equal to 2, so
-// we console.log 'yes'.
+// 'yes' is logged to the console.
 
 //CODE
 /*
@@ -26,10 +26,12 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// Line 36: Declares a variable as string 'Dane'
-// Line 38: Begins a conditional statement block. The block checks if the variable 'name' is equivalent
+// Line 38: Declares a variable as string 'Dane'
+// Line 40: Begins a conditional statement block. The block checks if the
+// variable 'name' is equivalent
 // in type and content to the string 'Mary'. If it is, 'Hi, Mary!' is logged to the console.
 // If the value of name is not 'Mary', 'How do you do?' is logged to the console.
+// In this case, 'How do you do?' is logged to the console.
 
 //CODE
 /*
@@ -45,13 +47,14 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// Line 57: Declares variable secret, it currently has 'null' value(?).
-// Line 59: Declares variable 'code', setting it's value to 123.
-// Line 61: Conditional statement, where if the value of var 'code' is equivalent to 123 in type and value
-// the value of var 'secret' is set to string 'super', and the value of var 'code' is equal to 246 (multiplied by its held value twice)
-// Line 66: The next conditional statement checks if var 'code' has a value higher than 250. If so,
+// Line 60: Declares variable secret, it currently has 'null' value(?).
+// Line 62: Declares variable 'code', setting its value to 123.
+// Line 64: Conditional statement, where if the value of var 'code' is equivalent
+// to 123 in type and value the value of var 'secret' is set to string 'super',
+// and the value of var 'code' is equal to 246 (multiplied by its held value twice)
+// Line 69: The next conditional statement checks if var 'code' has a value higher than 250. If so,
 // the value of var 'secret' is set to string 'duper'.
-// Line 70: logs the value of var 'secret', currently the string 'duper', to the console
+// Line 73: logs the value of var 'secret', currently the string 'duper', to the console
 //CODE
 /*
 let secret;
@@ -72,11 +75,12 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// Line 82-84 declares three variables, values Boolean true, and ints 34 & 55407
-// Line 86: nested conditional statement. First checks isStudent to true & zip > 80000
-// Both are not true, so it moves to the next block. if isStudent is false OR age <30.
-// Neither are true, so it checks if 'isStudent' is true. It is. 'Welcome to Prime!' is logged to the console'
-// Line 93 is an else statement that if none of the above three conditions were met, would print 'How about the weather?'.
+// Line 86-88 declares three variable//values isStudent//true, age//34, zip//55407
+// Line 90: nested conditional statement. First checks whether isStudent is true AND zip > 80000
+// Both are not true, L92 moves to the next block. if isStudent is false OR age <30.
+// Neither are true, L94 checks if 'isStudent' is true. It is.
+// 'Welcome to Prime!' is logged to the console'
+// Line 96 is an else statement that would print 'How about the weather?' if above conditions were false
 //CODE
 /*
 let isStudent = true;
@@ -94,9 +98,6 @@ if (isStudent === true && zip > 80000 ) {
 }
 
 */
-
-
-
 
 // REVERSE
 // Now, for questions 4-6, we'll give you a description and some code.
@@ -124,7 +125,6 @@ if (number >= 2) {
 }
 
 */
-
 
 
 //4. We start with three variables -- colorOne's value is set to string 'blue' and
